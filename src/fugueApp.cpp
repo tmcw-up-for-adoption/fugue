db800b9693fbe87afbd7fe35b8eb195ae605e143
Tom MacWright <tom@macwright.org>
2012-01-11 00:27:37 -0500

    Aesthetics

--- src/fugueApp.cpp
+++ src/fugueApp.cpp
@@ -10,17 +10,19 @@
 using namespace ci;
 using namespace ci::app;
 
-const int BS = 20;
-const int X = 32;
-const int Y = 16;
+const int BS = 40;
+const int X = 10;
+const int Y = 8;
 const int S = X * Y;
+const int BPM = 60;
+const double SPB = 60 / BPM;
 
 class fugueApp : public AppCocoaTouch {
-  public:
+public:
 	virtual void setup();
     virtual void sineWave(uint64_t inSampleOffset,
-                          uint32_t ioSampleCount,
-                          audio::Buffer32f *ioBuffer);
+      uint32_t ioSampleCount,
+      audio::Buffer32f *ioBuffer);
 	virtual void resize(ResizeEvent event);
 	virtual void update();
 	virtual void draw();
@@ -29,7 +31,7 @@ class fugueApp : public AppCocoaTouch {
 	float mPhase;
 	float mPhaseAdjust;
 	float mMaxFreq;
-  private:
+private:
     std::vector<int> notes;
 };
 
@@ -55,9 +57,6 @@ void fugueApp::sineWave( uint64_t inSampleOffset, uint32_t ioSampleCount, audio:
 	}
 }
 
-void fugueApp::resize(ResizeEvent event) {
-}
-
 void fugueApp::touchesBegan(TouchEvent event) {
     int idx = 
       (std::floor(event.getTouches()[0].getX() / BS) * X) +
@@ -69,43 +68,57 @@ void fugueApp::touchesBegan(TouchEvent event) {
     }
 }
 
-void fugueApp::update() {
-}
-
 void fugueApp::draw() {
     gl::setMatricesWindow(getWindowSize());
     gl::clear();
     gl::color(Color(1, 1, 1));
     
-    int secs = getElapsedSeconds() * 5;
-    int hlrow = secs % Y;
+    // std::clog << getElapsedSeconds() << std::endl;
     
-    // 16 40px blocks
+    double secs = getElapsedSeconds();
+    int hlrow = (int) std::floor(secs / SPB) % Y;
+    
+    std::clog << fmod(secs, Y * SPB) << std::endl;
+    
+    gl::color(Color(1, 0.5, 1));
+    gl::drawSolidRect(Rectf(fmod(secs, Y * SPB) * (BS), 0, fmod(secs, Y * SPB) * (BS) + 1, BS * X));
+    gl::color(Color(1, 1, 1));
+    
     for (int x = 0; x < X; x++) {
         gl::drawSolidRect(Rectf(0, x * BS, 320.0f, x * BS + 1));
     }
-    // 8 40px blocks
     for (int y = 0; y < Y; y++) {
-        if (y == hlrow) gl::color(Color(1, 0.5, 1));
-        gl::drawSolidRect(Rectf(y * BS, 0, y * BS + 1, 640.0f));
-        if (y == hlrow) gl::color(Color(1, 1, 1));
+        gl::drawSolidRect(Rectf(y * BS, 0, y * BS + 1, BS * X));
     }
+    gl::drawSolidRect(Rectf(Y * BS - 1, 0, Y * BS, BS * X));
     gl::color(Color(1, 1, 1));
     for (int n = 0; n < S; n++) {
         if (notes[n] == 1) {
             if (std::floor(n / X) == hlrow) {
+                double height = secs - std::floor(secs);
+                std::clog << height << std::endl;
+                std::clog << secs << std::endl;
                 gl::color(Color(1, 0.5, 1));
-                mFreqTarget = 440 * std::pow(2.0, ((n % X)) / 12.0);
-                std::clog << mFreqTarget << std::endl;
-            }
-            gl::drawSolidRect(Rectf(
+                gl::drawSolidRect(Rectf(
+                    std::floor(n / X) * BS + 1.0f,
+                    std::floor(n % X) * BS + 1.0f,
+                    std::floor(n / X) * BS + (BS * height),
+                    std::floor(n % X) * BS + BS));
+                gl::color(Color(1, 1, 1));
+                // mFreqTarget = 440 * std::pow(2.0, ((n % X)) / 12.0);
+            } else {
+                gl::drawSolidRect(Rectf(
                 std::floor(n / X) * BS + 1.0f,
                 std::floor(n % X) * BS + 1.0f,
                 std::floor(n / X) * BS + BS,
                 std::floor(n % X) * BS + BS));
-            if (std::floor(n / X) == hlrow) gl::color(Color(1, 1, 1));
+            }
         }
     }
 }
+
+void fugueApp::resize(ResizeEvent event) { }
+
+void fugueApp::update() { }
 
 CINDER_APP_COCOA_TOUCH(fugueApp, RendererGl)
